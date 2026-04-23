300f48e37bd07ce55af254b272bee32c240eb471
ashekookian1 <ashekookian1@my.brookdalecc.edu>
2026-04-23 01:41:01 -0400

    sql scripts added for answer and question tables

---
+++ DB/Questions table.sql
@@ -0,0 +1,37 @@
+/*
+-- Query: SELECT * FROM henry_VIII.quiz_questions
+LIMIT 0, 1000
+
+-- Date: 2026-04-23 01:32
+*/
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (1,'Who is wife #2?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (2,'When was Henry crowned King?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (3,'Who was wife #6?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (4,'Who was wife #3?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (5,'Who was wife #5?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (6,'Who was wife #1?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (7,'Who was wife #4?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (8,'Who was Katherine of Aragon\'s first husband?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (9,'Who was Henry\'s younger daughter?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (10,'Who was Henry\'s older daughter?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (11,'Who was Elizabeth I\'s mother?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (12,'Who was Mary I\'s mother?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (13,'Who was Edward VI\'s mother?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (14,'Who was Henry FitzRoy\'s mother?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (15,'Who declared Henry VIII\'s marriage to Katherine of Aragon as null and void?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (16,'What year was Anne Boleyn crowned Queen of England?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (17,'Who arranged Henry\'s marriage to Anne of Cleves?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (18,'Who was Katherine Parr\'s second husband?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (19,'Who was King of Spain, Holy Roman Emperor, nephew to Katherine of Aragon and first cousin to Mary I?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (20,'Who did Catherine Howard have an affair with when she was married to Henry?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (21,'Who helped hide the affair between Thomas Culpeper and Catherine Howard from Henry VIII?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (22,'Who was Katherine Parr\'s fourth husband?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (23,'When did Henry VIII die?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (24,'Who did Edward VI name as his successor?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (25,'Who was Mary I\'s husband?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (26,'Who burned 280 Protestants at the stake for heresy - including former Archbishop Thomas Cranmer - during their reign?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (27,'Who was Elizabeth I thought to be in love with?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (28,'Whom did Elizabeth I marry?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (29,'Who was probably the most well known execution of Elizabeth I\'s reign?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (30,'Who sent an armada to invade England that was defeated by Elizabeth I\'s navy?',1);
+INSERT INTO `` (`question_id`,`question`,`points`) VALUES (31,'Which US state is named after Elizabeth I? ',1);
